aedefea1ee6346de0db4d1e5f2ccceeeb7269b4b
Keiichiro Sato <10kmartinis29@gmail.com>
2023-01-18 23:59:28 +0900

    feat: Add BaseHead component

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://blog.keisatoh.net',
   integrations: [tailwind(), svelte()],
   markdown: {
     shikiConfig: {
