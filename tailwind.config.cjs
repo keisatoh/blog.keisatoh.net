41f2dff646bc673bb7e3ba834da7cbecb3ff6e99
Keiichiro Sato <10kmartinis29@gmail.com>
2023-08-26 23:51:39 +0900

    change: add Layout component and tweak some styles
    - Layoutコンポーネントの追加
      - 記事ページにもヘッダーが表示されるようにした
    
    主なスタイルの変更
    - dekや本文のfont weightを軽めにした
      - 試験的
    - トップ・記事ページのmax-widthを揃えた
      - 左揃えにしたヘッダーを同位置にするため
      - 記事ページにあてたproseクラスがmax-widthを持っているので、それに合わせた

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: '#888888',
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
