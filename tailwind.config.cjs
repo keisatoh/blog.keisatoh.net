d5c3347c1a16265486b94dd987253bfd96ffd3b7
Keiichiro Sato <10kmartinis29@gmail.com>
2023-08-31 21:06:04 +0900

    feat: add prose blockquote style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,26 @@ module.exports = {
       colors: {
         gray: '#888888',
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            blockquote: {
+              fontWeight: theme('fontWeight.medium'),
+              color: theme('colors.gray'),
+              marginTop: '36px',
+              marginBottom: '36px',
+              paddingLeft: '20px',
+              fontWeight: '400',
+            },
+            'blockquote p:first-of-type::before': {
+              content: '',
+            },
+            'blockquote p:last-of-type::after': {
+              content: '',
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [require('@tailwindcss/typography')],
